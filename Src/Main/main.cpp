995cf6b4a0bcd057230c7cdded24390924e110c7
edoardo vocaturo <edoardovocaturo@gmail.com>
2026-05-04 13:49:00 +0200

    remove

--- Src/Main/main.cpp
+++ Src/Main/main.cpp
@@ -31,36 +31,11 @@ PLATFORM_MAIN
 	manager.SetMinWindowSize(800, 600);
 	manager.MaximizeWindow();
 
-	const int activeFPS = 60;
-	const int idleFPS   = 5;
-
 	while (!manager.ShouldQuit())
 	{
-		auto frameStart = std::chrono::high_resolution_clock::now();
-
 		manager.StartDrawScene();
 		ImGui::ShowDemoWindow();
 		manager.EndDrawScene();
-
-		ImGuiIO& io = ImGui::GetIO();
-
-		bool isActive =
-			io.WantCaptureMouse ||
-			io.WantCaptureKeyboard ||
-			io.MouseDelta.x != 0.0f ||
-			io.MouseDelta.y != 0.0f ||
-			ImGui::IsAnyItemActive() ||
-			ImGui::IsAnyItemHovered();
-
-		int targetFPS = isActive ? activeFPS : idleFPS;
-
-		auto frameTime = std::chrono::milliseconds(1000 / targetFPS);
-
-		auto frameEnd = std::chrono::high_resolution_clock::now();
-		auto elapsed  = frameEnd - frameStart;
-
-		if (elapsed < frameTime)
-			std::this_thread::sleep_for(frameTime - elapsed);
 	}
 
 	return 0;
